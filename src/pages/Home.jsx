0b65dbc5bc8923f57f1418a542424a55a86e4624
rizkilabs <rizkilabs.dev@gmail.com>
2025-11-23 10:51:23 +0700

    feat: add simple Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <div className="text-center">
+      <h1 className="fw-bold mb-3">Selamat Datang di SiswaApp</h1>
+      <p className="lead">
+        Aplikasi sederhana untuk mengelola data siswa. Gunakan menu di atas
+        untuk mulai CRUD data siswa.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
