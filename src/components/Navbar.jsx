32798d2ba35c74119eda72fabd4c9f3e738fd2f9
rizkilabs <rizkilabs.dev@gmail.com>
2025-11-23 10:44:18 +0700

    feat: add responsive Navbar component with routing links

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,63 @@
+import { Link, NavLink } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        {/* Logo */}
+        <Link className="navbar-brand fw-bold" to="/">
+          SiswaApp
+        </Link>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarContent"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Menu */}
+        <div className="collapse navbar-collapse" id="navbarContent">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  "nav-link" + (isActive ? " active fw-bold" : "")
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink
+                to="/siswa"
+                className={({ isActive }) =>
+                  "nav-link" + (isActive ? " active fw-bold" : "")
+                }
+              >
+                Data Siswa
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink
+                to="/siswa/create"
+                className={({ isActive }) =>
+                  "nav-link" + (isActive ? " active fw-bold" : "")
+                }
+              >
+                Tambah Siswa
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
